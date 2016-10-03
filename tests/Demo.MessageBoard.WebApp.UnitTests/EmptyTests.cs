09139fdab3aa9499f0123f75aade45992f25fa23
Kevin Herr <kherr@GCMLP.COM>
2016-10-03 14:24:02 -0500

    init unit test proj

---
+++ tests/Demo.MessageBoard.WebApp.UnitTests/EmptyTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Demo.MessageBoard.WebApp.UnitTests
+{
+    [TestFixture]
+    public class EmptyTests
+    {
+        [Test]
+        public void TestStringIsEmpty()
+        {
+            // Arrange
+            var expected = string.Empty;
+
+            // Act
+            var actual = "";
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
